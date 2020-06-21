489d767feaffc5d755148f6a25fcc225638bf52d
Dmitrii Lipin <asar90@bk.ru>
2020-06-21 20:50:00 +0300

    Добавление классов и объектов, конструктор,  this

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -2,9 +2,22 @@ package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
   public static void main(String[] args) {
-    System.out.println("Hello, word");
+    hello("world");
+    hello("user");
+    hello("Dmitrii");
 
+    Square s = new Square(5);
+
+    System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
+
+    Rectangle r = new Rectangle(5,6);
+    System.out.println("Площадь прямоугольник со стороной " + r.a + " и " +  r.b + " = " + r.area());
   }
+
+  public static void hello(String somebody) {
+    System.out.println("Hello, "  + somebody + "!");
+  }
+
 }
 
 
